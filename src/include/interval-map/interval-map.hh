3c0ddf3021bad052cc61169245521232183f9e5a
Bruno BELANYI <bruno@belanyi.fr>
2024-08-23 21:40:22 +0100

    Add given code

--- src/include/interval-map/interval-map.hh
+++ src/include/interval-map/interval-map.hh
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <map>
+
+namespace amby {
+
+template <typename K, typename V> class interval_map {
+public:
+    interval_map(V const& init) : init_(init) {}
+
+    void assign(K const& begin, K const& end, V const& val) {
+        // TODO: implement
+    }
+
+    V const& operator[](K const& key) const {
+        auto it = underlying_.upper_bound(key);
+        if (it == underlying_.begin())
+            return init_;
+        return std::prev(it)->second;
+    }
+
+private:
+    V init_;
+    std::map<K, V> underlying_{};
+};
+
+} // namespace amby
